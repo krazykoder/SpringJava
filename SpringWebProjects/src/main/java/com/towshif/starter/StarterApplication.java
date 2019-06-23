4dcad191e2b47710e01be6f3ad9981a8a99029dd
krazyKoder <towshif@gmail.com>
2019-06-23 12:17:00 -0700

    models added

--- SpringWebProjects/src/main/java/com/towshif/starter/StarterApplication.java
+++ SpringWebProjects/src/main/java/com/towshif/starter/StarterApplication.java
@@ -3,6 +3,7 @@ package com.towshif.starter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
 
 @ComponentScan("com.towshif")
